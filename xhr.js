ff81a4d1b15e79fa146cc77eca0ae52e1e8a4112
Raekweh <Raekweh009@gmail.com>
2022-05-06 17:46:53 +1200

    creating xhr.js file

---
+++ xhr.js
@@ -0,0 +1,11 @@
+// The file xhr.js will create a link to the browser.
+function createRequest() {
+    var xhr = false;  
+    if (window.XMLHttpRequest) {
+        xhr = new XMLHttpRequest();
+    }
+    else if (window.ActiveXObject) {
+        xhr = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    return xhr;
+}
