983a77983ca11e13f490860f9b75fb85a55ffee4
Raymond Li <raekweh@Raymonds-MacBook-Air-2.local>
2022-05-09 16:35:32 +1200

    booking form critria

--- bookingServer.php
+++ bookingServer.php
@@ -1,10 +1,10 @@
 <?php
-
+	echo "<h1> Thank you for your booking!</h1>";
 	// get name and password passed from client
 	$customerName = $_POST['name'];
 	$phoneNumber = $_POST['phone'];
 	// sleep for 10 seconds to slow server response down
 	sleep(3);
 	// write back the password concatenated to end of the name
-	echo ($customerName." : ".$phoneNumber);
+	echo "<p> $customerName : $phoneNumber.</p>";
 ?>
