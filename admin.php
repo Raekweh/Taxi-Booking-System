6a4097ea11f3a8e19bdf1832d098609ed17a60cd
Raymond Li <raekweh@Raymonds-MacBook-Air-2.local>
2022-05-28 10:21:28 +1200

    successful db connection

--- admin.php
+++ admin.php
@@ -1,6 +1,21 @@
 <?php
-    sleep (3);
-    echo "<p>Testing js connection</p>";
-    $bookingSearch = $_POST['bookingsearch'];
-    echo "THe booking search is: ".$bookingSearch;
+    require_once('./conf/sqlinfo.php');
+
+    $conn = @mysqli_connect(
+        $sql_host,
+        $sql_user,
+        $sql_pass,
+        $sql_db
+    );
+
+    if(!$conn)
+    {
+        echo "<p>Failed to connect to database</p>";
+    }
+    else{
+        sleep (3);
+        echo "<p>Database connection is successful</p>";
+        $bookingSearch = $_POST['bookingsearch'];
+        echo "The booking search is: ".$bookingSearch;
+    }
 ?>
