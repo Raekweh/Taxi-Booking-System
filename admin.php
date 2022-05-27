409f04d3cf67fe7432d904e5f3fb86e38667f1d6
Raymond Li <raekweh@Raymonds-MacBook-Air-2.local>
2022-05-28 10:48:34 +1200

    valid BRN check

--- admin.php
+++ admin.php
@@ -10,12 +10,39 @@
 
     if(!$conn)
     {
-        echo "<p>Failed to connect to database</p>";
+        echo "<p>Failed to connect to database.</p>";
     }
     else{
         sleep (3);
         echo "<p>Database connection is successful</p>";
-        $bookingSearch = $_POST['bookingsearch'];
-        echo "The booking search is: ".$bookingSearch;
+
+        if(validBRN($_POST['bookingsearch']))
+        {
+            $bookingSearch = $_POST['bookingsearch'];
+            echo "The booking search is: ".$bookingSearch;
+        }
+    }
+
+    //Checking if the user inputs the bookking number reference in the correct format.
+    function validBRN($bookingNumber)
+    {
+        if(empty($bookingNumber) || !isset($bookingNumber))
+        {
+            echo "<p>No Records of Booking Number.</p>";
+            return false;
+        }
+        else{
+            $pattern = "/BRN+[0-9]{5}/";
+
+            if(preg_match($pattern, $bookingNumber))
+            {
+                return true;
+            }
+            else
+            {
+                echo "<p>Please input in the booking number in the format BRN00000.</p>";
+                return false;
+            }
+        }
     }
 ?>
