1b3b0eb138d4fd87235e383b60147eeeac2b9d79
Raymond Li <raekweh@Raymonds-MacBook-Air-2.local>
2022-06-03 09:59:00 +1200

    2 hours table

--- admin.php
+++ admin.php
@@ -30,7 +30,8 @@
                 $subString = substr($bookingSearch,3);
                 $bookingNumber = (int) $subString;
 
-                echo "<p>The BRN is in the correct format</p>";
+                // echo "<p>The BRN is in the correct format</p>";
+
                 //Finding the Reference Number
                 $searchQuery = "SELECT * FROM $sql_tble WHERE ReferNumber = $bookingNumber";
                 $gettingBRN = "SELECT * FROM $sql_tble WHERE ReferNumber = $bookingNumber";
@@ -45,7 +46,7 @@
             //Checks if the booking reference is empty or a space
             else if(empty($_POST['bookingsearch'])  || $_POST['bookingsearch'] == " ")
             {
-                echo "<p>Empty String</p>";
+                // echo "<p>Empty String</p>";
                 $currentTime = date("H:i");
                 // echo "<p>The current time is $currentTime </p>";
                 $twoHours = date("H:i",strtotime("2 hours", strtotime($currentTime)));
@@ -55,24 +56,13 @@
                 //Select the pick up time between current and two hours after
                 $searchQuery = "SELECT * FROM $sql_tble WHERE PickupTime BETWEEN '$currentTime' AND '$twoHours' AND PickupDate = '$currentDate'";
                 $BRNQuery = "SELECT ReferNumber FROM $sql_tble WHERE PickupTime BETWEEN '$currentTime' AND '$twoHours' AND PickupDate = '$currentDate'";
-                echo $searchQuery;
-                // echo $searchQuery;
     
                 //Executing MySQL Commands
                 $searchResults = @mysqli_query($conn, $searchQuery);
                 $BRNResults = @mysqli_query($conn, $BRNQuery);
 
-
-                //Checking if the search results work //REmove later
-                if(!$searchResults)
-                {
-                    echo "<p>The time period does not work</p>";
-
-                }
-                else{
-                    echo "<p>The time period is working</p>";
-                    displayTwoHTable($searchResults, $BRNResults);
-                }
+                //Displaying the table
+                displayTwoHTable($searchResults, $BRNResults);
             }
         }
         else{
@@ -87,8 +77,33 @@
         //Checking if the queries work
         if($searchResults && $BRNResults)
         {
-            echo "<p>We run them streets</p>";
-        }
+
+                $BRNstring = "BRN00000";
+                //Generating a table
+                echo "<table width='100%' border='1'>";
+                echo "<tr>
+                <th>Booking Reference Number</th><th>Customer Name</th><th>Phone Number</th>
+                <th>Unit Number</th><th>Street Number</th> <th>Street Name</th>
+                <th>Destination Suburb</th> <th>Pickup Date</th><th>Pickup Time</th>
+                <th>Status</th><th>Assigned</th> 
+                </tr>";
+                //Displaying the table of content
+                while($row = mysqli_fetch_assoc($searchResults))
+                {
+                    echo "<tr><td>", substr($BRNstring,0, strlen($BRNstring) - strlen($row['ReferNumber'])) . $row['ReferNumber'],"</td>";
+                    echo "<td>",$row["CustomerName"],"</td>";
+                    echo "<td>",$row["PhoneNumber"],"</td>";
+                    echo "<td>",$row["UnitNumber"],"</td>";
+                    echo "<td>",$row["StreetNumber"],"</td>";
+                    echo "<td>",$row["StreetName"],"</td>";
+                    echo "<td>",$row["DestinationSuburb"],"</td>";
+                    echo "<td>",date('d/m/Y', strtotime($row['PickupDate'])),"</td>";
+                    echo "<td>",date("G:i", strtotime($row["PickupTime"])),"</td>";
+                    echo "<td>",$row["Status"],"</td>";
+                    echo "<td><input type='button' name ='changeAssigned' value='Assign'></td></tr>"; //Need to implement an onclick method to alter the data
+                }
+                echo "</table>";
+            }
     }
 
     //DIsplays the selected BRN
