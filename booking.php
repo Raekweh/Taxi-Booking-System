9c5ec6b36d6d4340b502cf0e2afb22df265fd416
Raekweh <Raekweh009@gmail.com>
2022-06-04 14:20:51 +1200

    date and time validation

--- booking.php
+++ booking.php
@@ -19,43 +19,35 @@
 		// sleep for 3 seconds to slow server response down
 		sleep(3);
 
-		// Check for validation
-		if(validPhone($_POST['phone']) && validsNumber($_POST['streetnumber']) && validstName($_POST['streetname']) && validpickupDate($_POST['pickupdate']) && validpickupTime($_POST['pickuptime']))
+		//Posts the variables
+		$customerName = $_POST['name'];
+		$phoneNumber = $_POST['phone']; //Need to change the database to varchar
+		$unitNumber = $_POST['unitnumber']; //Need to change the database to varchar
+		$streetNumber = $_POST['streetnumber'];
+		$streetName = $_POST['streetname'];
+		$suburbName = $_POST['suburb'];
+		$desintationSuburb = $_POST['destinationsuburb'];
+		$pickupDate = $_POST['pickupdate'];
+		$pickupTime = $_POST['pickuptime'];
+
+		//Inserting Command
+		$insert_sql = "INSERT INTO $sql_tble (CustomerName, PhoneNumber, UnitNumber, StreetNumber, StreetName, Suburb, DestinationSuburb, PickupDate, PickupTime, Status)
+		VALUES ('$customerName' ,'$phoneNumber', '$unitNumber', '$streetNumber', '$streetName', '$suburbName', '$desintationSuburb', '$pickupDate', '$pickupTime', 'Unassigned')";
+		$existenceResults = @mysqli_query($conn, $tableExistence);
+		//Validation results
+		if ($existenceResults !== FALSE) 
 		{
-			//Posts the variables
-			$customerName = $_POST['name'];
-			$phoneNumber = $_POST['phone']; //Need to change the database to varchar
-			$unitNumber = $_POST['unitnumber']; //Need to change the database to varchar
-			$streetNumber = $_POST['streetnumber'];
-			$streetName = $_POST['streetname'];
-			$suburbName = $_POST['suburb'];
-			$desintationSuburb = $_POST['destinationsuburb'];
-			$pickupDate = $_POST['pickupdate'];
-			$pickupTime = $_POST['pickuptime'];
-
-			echo "The pick up time is : $pickupTime";
-			//Inserting Command
-			$insert_sql = "INSERT INTO $sql_tble (CustomerName, PhoneNumber, UnitNumber, StreetNumber, StreetName, Suburb, DestinationSuburb, PickupDate, PickupTime, Status)
-			VALUES ('$customerName' ,'$phoneNumber', '$unitNumber', '$streetNumber', '$streetName', '$suburbName', '$desintationSuburb', '$pickupDate', '$pickupTime', 'Unassigned')";
-
-			$existenceResults = @mysqli_query($conn, $tableExistence);
-
-			//Validation results
-			if ($existenceResults !== FALSE) 
-			{
-    			//Executing the insert query
-				$insertingResult = @mysqli_query($conn, $insert_sql);
-
-				displayingBRN($conn, $latest_refNumber_query, $refereNumber_query);
-			} 
-			else
-			{
-				$creatingTableResult = @mysqli_query($conn,$creatingTable);
-				if($creatingTableResult !== FALSE)
+    		//Executing the insert query
+			$insertingResult = @mysqli_query($conn, $insert_sql);
+			displayingBRN($conn, $latest_refNumber_query, $refereNumber_query);
+		} 
+		else
+		{
+			$creatingTableResult = @mysqli_query($conn,$creatingTable);
+			if($creatingTableResult !== FALSE)
 				{
-					$insertingResult = @mysqli_query($conn, $insert_sql);
-					displayingBRN($conn, $latest_refNumber_query, $refereNumber_query);
-				}
+				$insertingResult = @mysqli_query($conn, $insert_sql);
+				displayingBRN($conn, $latest_refNumber_query, $refereNumber_query);
 			}
 		}
 	}
@@ -89,82 +81,3 @@
 		}
 		echo "</table>";
 	}
-
-
-	//Street name Validation
-	function validstName($stname)
-	{
-	    //Checks if the date is null or empty.
-		if (empty($stname) || !isset($stname)) 
-		{
-			echo "<p>Please enter a street name.</p>";
-			return false;
-		}
-		else
-		{
-			$pattern = "/^[a-zA-Z\s]*$/";
-			//Street name should only contain characters and space.
-			if(preg_match($pattern, $stname))
-			{
-				return true;
-			}
-			else
-			{
-				echo "<p>The street name box is incorrect </br>
-				Your street name must not include any special character nor numbers</p>";
-			}
-		}
-	}
-
-    //Pick up date validation. //Need a condition to check if the date is after the current date
-    function validpickupDate($date)
-    {
-		//Getting the current date
-		$currentDate = date("Y-m-d");
-        //Checks if the date is null or empty.
-        if (empty($date) || !isset($date)) 
-        {
-            echo "<p>Please insert a pick up date</p>";
-            return false;
-        }
-		else
-		{
-			//Checks if the date is the current or future date
-			if(strtotime($date) >= strtotime($currentDate))
-			{
-				return true;
-			}
-			else
-			{
-				echo "<p>Please select a date from $currentDate onwards</p>";
-				return false;
-			}		
-		}
-    }
-
-    //Pick up time validation. //Need to add acondition to check if the time is after the current time
-    function validpickupTime($time)
-    {
-    	$currentTime = date("H:i");
-        //Checks if the date is null or empty.
-        if (empty($time) || !isset($time)) 
-        {
-            echo "<p>Please insert a pick up date</p>";
-            return false;
-        }
-        else
-        {
-			//Checks if the time inserted is after the current time
-                if(strtotime($time) >= strtotime($currentTime))
-                {
-					echo "<p>This is working somehow </p>";
-                	return true;
-                }
-                else
-                {
-                	echo "<p>Please select a time before the current time $currentTime</p>";
-                	return false;
-                }
-        }
-    }
-?>
--- bookingComponents/booking.js
+++ bookingComponents/booking.js
@@ -1,20 +1,18 @@
 //
 var xhr = createRequest();
 var name
-function getData(dataSource, divID, cname, mphone, unumber, snumber, stname,sbname, dsbname, pdate, ptime) 
-{
-   if(validName(cname) && validPhone(mphone) && validSNumber(snumber) )
-  {
+function getData(dataSource, divID, cname, mphone, unumber, snumber, stname, sbname, dsbname, pdate, ptime) {
+  if (validName(cname) && validPhone(mphone) && validSNumber(snumber) && validStNumber(stname) && validPickupDateTime(pdate,ptime)) {
     if (xhr) {
       var obj = document.getElementById(divID);
       var requestbody = "name=" + encodeURIComponent(cname) + "&phone=" + encodeURIComponent(mphone)
         + "&unitnumber=" + encodeURIComponent(unumber) + "&streetnumber=" + encodeURIComponent(snumber)
         + "&streetname=" + encodeURIComponent(stname) + "&suburb=" + encodeURIComponent(sbname)
-        + "&destinationsuburb=" + encodeURIComponent(dsbname) + "&pickupdate=" +encodeURIComponent(pdate)
-        + "&pickuptime=" +encodeURIComponent(ptime);
+        + "&destinationsuburb=" + encodeURIComponent(dsbname) + "&pickupdate=" + encodeURIComponent(pdate)
+        + "&pickuptime=" + encodeURIComponent(ptime);
       xhr.open("POST", dataSource, true);
       xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-  
+
       xhr.onreadystatechange = function () {
         alert(xhr.readyState); // to let us see the state of the computation 
         if (xhr.readyState == 4 && xhr.status == 200) {
@@ -23,26 +21,23 @@ function getData(dataSource, divID, cname, mphone, unumber, snumber, stname,sbna
       } // end anonymous call-back function 
       xhr.send(requestbody);
     } // end if  
- }
-  
+  }
+
 } // end function getData()
 
 //Customer Name Validation
-function validName(cname)
-{
+function validName(cname) {
   var pattern = /^[a-zA-Z\s]*$/;
   //Checks if the name is null
-  if(cname == null || cname.length == 0)
-  {
+  if (cname == null || cname.length == 0) {
     alert("Please enter a customer name");
     return false;
   }
-  else{
-    if(cname.match(pattern))
-    {
+  else {
+    if (cname.match(pattern)) {
       return true;
     }
-    else{
+    else {
       alert("Please change the format of your customer name. Please do not inlude numbers or special characters");
       return false;
     }
@@ -50,42 +45,36 @@ function validName(cname)
 }
 
 //Phonee Validation
-function validPhone(mphone)
-{
+function validPhone(mphone) {
   var pattern = /^[0-9]*$/;
 
-  if(mphone == null || mphone.length == 0)
-  {
+  if (mphone == null || mphone.length == 0) {
     alert("Please enter a phone number");
     return false;
   }
-  else
-  {
+  else {
 
-    if(mphone.match(pattern))
+    if (mphone.match(pattern)) 
     {
-      if(mphone.length >= 10 && mphone.length <= 12)
-      {
+      if (mphone.length >= 10 && mphone.length <= 12) {
         return true;
       }
-      else{
+      else {
         alert("Please ensure that your phone number is between 10 to 12 digits long");
+        return false;
       }
     }
-    else{
-
+    else {
       alert("Please ensure that your phone number is numbers");
+      return false;
     }
-
-
+    return false;
   }
 }
 
 //Street Number validation
-function validSNumber(snumber)
-{
-  if(snumber == null || snumber.length == 0)
-  {
+function validSNumber(snumber) {
+  if (snumber == null || snumber.length == 0) {
     alert("Please enter a street number");
     return false;
   }
@@ -93,45 +82,37 @@ function validSNumber(snumber)
 }
 
 //Street Name validation
-function validStNumber(stname)
-{
+function validStNumber(stname) {
   var pattern = /^[a-zA-Z\s]*$/;
-  if(stname == null || stname.length == 0)
-  {
+  if (stname == null || stname.length == 0) {
     alert("Please enter a street name");
     return false;
   }
-  else{
-    if(stname.match(patter))
-    {
+  else {
+    if (stname.match(pattern)) {
       return true;
     }
-    else{
+    else {
       alert("Please ensure that your street name does not include any number or special characters");
       return false;
     }
   }
 }
 //
-function validPickupDate(pdate, ptime)
-{
-  var currentDate = new Date().toISOString;
-  var input = new Date(pdate).toISOString;
+function validPickupDateTime(pdate, ptime) {
+  if (pdate != null || ptime != null) {
+    //Converting the current date to
+    var currentDate = new Date();
+    var cd = Date.parse(currentDate);
 
-  var currentDateObject = new Date()
+    enteredDateString = pdate + " " + ptime;
+    var enteredDate = Date.parse(enteredDateString);
 
-  if(input == null)
-  {
-    alert("Please select a date");
-    return false;
-  }
-  else{
-    if (input >= currentDate)
-    {
+    if (enteredDate >= cd) {
       return true;
     }
-    else{
-      alert("Please select a date from the current date on wards");
+    else {
+      alert("Please ensure that the date or time is after the current date and the current time");
     }
   }
 }
